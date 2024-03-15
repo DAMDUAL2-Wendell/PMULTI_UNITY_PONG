34077f0d5dcae7f8da1c887b1b835e59ff1a069d
DAMDU202 <DAMDU202@A32PC14>
2024-03-15 20:00:25 +0100

    agregada pantalla help y tiempo de partida

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,14 +1,19 @@
+using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Ball : MonoBehaviour
 {
-    public float speed = 10f;
-    public float maxSpeed = 40f;
-    public float speedIncreaseAmount = 2f;
-    public float tiempoAumentarVelocidad = 1f;
-    private float tiempoTranscurrido = 0f;
+    public float INITIAL_SPEED = 8f;
+    public float SPEED = 8f;
+    public float MAX_SPEED = 30f;
+    public float SPEED_INCREASE_AMOUNT = 0.5f;
+    public float TIME_INCREASE_SPEED = 0.5f;
+    private float elapsedTime = 0f;
+
+    public float tiempoInicial = 5f;
+    public float tiempoRestante;
 
     // Posición Inicial de la pelota en el centro, X e Y
     private Vector2 posInicial = new Vector2(0f, 0f);
@@ -19,82 +24,180 @@ public class Ball : MonoBehaviour
     public Text redTextScore;
     public Text blueTextScore;
 
+    public Text reloj;
+
     public Text notificationText;
 
     private Rigidbody2D rb;
 
     void Start()
     {
+
+        // Obtener el RigidBody2d del componente actual que es la Bola.
         rb = GetComponent<Rigidbody2D>();
-        rb.velocity = Vector2.right * speed;
+
+        // Asignar a la Bola un Vector 2 con dirección derecha y velocidad 'SPEED'.
+        rb.velocity = Vector2.right * SPEED;
 
         // Encuentra los objetos Text para el marcador
         redTextScore = GameObject.Find("redTextScore").GetComponent<Text>();
         blueTextScore = GameObject.Find("blueTextScore").GetComponent<Text>();
+
+        reloj = GameObject.Find("reloj").GetComponent<Text>();
+
+        // Objeto text para mostrar la notificación final de quién ha ganado.
         notificationText = GameObject.Find("notificationText").GetComponent<Text>();
+
+        // Hacer inactivo el objeto de notificación
         notificationText.gameObject.SetActive(false);
+
+        tiempoRestante = tiempoInicial;
+
+        
     }
 
+    private void ActualizarHora(){
+        // Actualizar el tiempo restante
+        tiempoRestante -= Time.deltaTime;
+
+        // Convertir el tiempo restante a minutos y segundos
+        int minutos = Mathf.FloorToInt(tiempoRestante / 60);
+        int segundos = Mathf.FloorToInt(tiempoRestante % 60);
+
+        // Actualizar el texto del contador
+        reloj.text = string.Format("{0:00}:{1:00}", minutos, segundos);
+
+        // Verificar si el tiempo ha llegado a cero
+        if (tiempoRestante <= 0f)
+        {
+            tiempoRestante = 0f;
+
+           // Parar la bola
+            rb.velocity = Vector2.zero;
+
+            // Reiniciar el tiempo
+            reloj.text = "03:00"; // Establecer el tiempo inicial
+            tiempoRestante = tiempoInicial; // Reiniciar el tiempo restante
+
+
+            ComprobarGanador();
+            reiniciarPartida();
+
+
+            Debug.Log("¡Tiempo agotado!");
+        }
+    }
+
+    // Método para reiniciar la pelota a la posición central y establecer la velocidad a 0.
     void reiniciarPelota()
     {
         rb.position = posInicial;
         rb.velocity = velocidadParada;
+        SPEED = INITIAL_SPEED;
     }
 
+   
+
+    // Método Update que se actualiza en cada Frame del renderizado.
     private void Update()
     {
-        tiempoTranscurrido += Time.deltaTime;
-        if (tiempoTranscurrido >= tiempoAumentarVelocidad)
+        // Aumentar el tiempo en la variable elapsedTime
+        elapsedTime += Time.deltaTime;
+
+        // Si el tiempo transcurrido es superior o igual a un tiempo definido en TIME_INCREASE_SPEED, aumentamos la velocidad de la pelota.
+        if (elapsedTime >= TIME_INCREASE_SPEED)
         {
-            if (speed < maxSpeed)
+            if (SPEED < MAX_SPEED)
             {
-                speed += speedIncreaseAmount;
+                SPEED += SPEED_INCREASE_AMOUNT;
             }
-            tiempoTranscurrido = 0f;
+            elapsedTime = 0f;
         }
 
+        // Si la pelota está en posición central y su velocidad es 0 comprobamos si se pulsa la tecla espacio para comenzar el movimiento de la pelota.
         if (rb.position == posInicial && rb.velocity == velocidadParada)
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                rb.velocity = Vector2.right * speed;
+                rb.velocity = Vector2.right * SPEED;
             }
         }
 
-        if (bluescore >= 10)
-        {
-            ShowNotification("Enhorabuena Jugador1, has ganado!");
-            reiniciarPartida();
-        }
-        if (redscore >= 10)
-        {
-            ShowNotification("Enhorabuena Jugador1, has ganado!");
-            reiniciarPartida();
-        }
+        ComprobarMarcadores();
+        ActualizarHora();
 
     }
 
+    private void ComprobarMarcadores(){
+        // Finalizar el juego si el marcador del jugador uno llega a 5.
+        if (bluescore >= 5)
+        {
+            ShowNotification("Enhorabuena Jugador1, has ganado!\nPara jugar otra vez pulsar espacio.");
+
+            // Reiniciar partida (Hay que pulsar espacio para que reinicie).
+            reiniciarPartida();
+
+        }
+
+        // Finalizar el juego si el marcador del jugador dos llega a 5.
+        if (redscore >= 5)
+        {
+            ShowNotification("Enhorabuena Jugador2, has ganado!\nPara jugar otra vez pulsar espacio.");
+
+            // Reiniciar partida (Hay que pulsar espacio para que reinicie).
+            reiniciarPartida();
+        }
+    }
+
+    private void ComprobarGanador(){
+        if(bluescore == 0 && redscore == 0){
+            ShowNotification("Los marcadores están a 0, nadie ha ganado.\nPara jugar otra vez pulsar espacio.");
+        }
+        if(bluescore > redscore){
+            ShowNotification("Enhorabuena Jugador1, has ganado!\nPara jugar otra vez pulsar espacio.");
+        }
+        if(redscore > bluescore){
+            ShowNotification("Enhorabuena Jugador2, has ganado!\nPara jugar otra vez pulsar espacio.");
+        }
+    }
+
+    // Método para mostrar una notificación en el Objeto Text de notificación.
     private void ShowNotification(string message)
     {
         notificationText.text = message;
         notificationText.gameObject.SetActive(true);
     }
 
+    private void HideNotification(){
+        notificationText.gameObject.SetActive(false);
+    }
+
+    // Método para reiniciar la partida, primero reinicia la pelota a la posición central y luego reinicia los marcadores.
     private void reiniciarPartida()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            reiniciarPelota();
             reiniciarMarcadores();
+            ActualizarLabelsMarcadores();
+            HideNotification();
+            reloj.text = "03:00";
+            tiempoRestante = tiempoInicial;
         }
     }
 
+    private void ActualizarLabelsMarcadores(){
+        redTextScore.text = "0";
+        blueTextScore.text = "0";
+    }
+
+    // Método para reinciar los Text a 0.
     private void reiniciarMarcadores()
     {
         redscore = 0;
         bluescore = 0;
     }
 
+    // Método para cambiar de color un GameObject con una duración determinada.
     IEnumerator ChangeColorAndRestore(GameObject obj, Color newColor, float duration)
     {
         SpriteRenderer spriteRenderer = obj.GetComponent<SpriteRenderer>();
@@ -107,23 +210,23 @@ public class Ball : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D other)
     {
         // Incrementa la velocidad en cada colisión
-        speed += speedIncreaseAmount;
+        SPEED += SPEED_INCREASE_AMOUNT;
 
         // Limita la velocidad máxima
-        speed = Mathf.Min(speed, maxSpeed);
+        SPEED = Mathf.Min(SPEED, MAX_SPEED);
 
         // Aplica la nueva velocidad a la pelota
-        rb.velocity = rb.velocity.normalized * speed;
+        rb.velocity = rb.velocity.normalized * SPEED;
 
         if (other.gameObject.name == "paddleBlue")
         {
             Vector2 dir = new Vector2(1, hitFactor(transform.position, other.transform.position, other.collider.bounds.size.y)).normalized;
-            rb.velocity = dir * speed;
+            rb.velocity = dir * SPEED;
         }
         else if (other.gameObject.name == "paddleRed")
         {
             Vector2 dir = new Vector2(-1, hitFactor(transform.position, other.transform.position, other.collider.bounds.size.y)).normalized;
-            rb.velocity = dir * speed;
+            rb.velocity = dir * SPEED;
         }
         else if (other.gameObject.name == "LeftWall")
         {
